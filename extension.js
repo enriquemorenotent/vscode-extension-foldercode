ceee0464895429f93ec9be8df1cf3fb989fa544a
Enrique Moreno Tent <enriquemorenotent@gmail.com>
2023-03-20 14:42:30 +0100

    Updates

--- extension.js
+++ extension.js
@@ -16,60 +16,82 @@ function generateMarkdownContent(files) {
 	return content;
 }
 
-async function readTextFiles(folderPath) {
+async function readTextFilesRecursive(folderPath, textFiles = []) {
 	const files = await fs.promises.readdir(folderPath);
-	const textFiles = [];
 
 	for (const file of files) {
 		const filePath = path.join(folderPath, file);
 		const fileStat = await fs.promises.stat(filePath);
 
 		if (fileStat.isFile()) {
-			const content = await fs.promises.readFile(filePath, 'utf-8');
+			let content = await fs.promises.readFile(filePath, 'utf-8');
+
+			// Crop the content if there are more than 500 lines
+			const lines = content.split('\n');
+			if (lines.length > 500) {
+				content = lines.slice(0, 500).join('\n');
+			}
+
 			textFiles.push({ name: file, content });
+		} else if (fileStat.isDirectory()) {
+			await readTextFilesRecursive(filePath, textFiles);
 		}
 	}
 
 	return textFiles;
 }
 
+async function readTextFiles(folderPath) {
+	return await readTextFilesRecursive(folderPath);
+}
+
+async function executeFolderCode() {
+	const folder = await vscode.window.showOpenDialog({
+		canSelectMany: false,
+		canSelectFiles: false,
+		canSelectFolders: true,
+		openLabel: 'Select Folder',
+	});
+
+	if (folder && folder[0]) {
+		try {
+			const textFiles = await readTextFiles(folder[0].fsPath);
+
+			// Check if there are more than 20 files
+			if (textFiles.length > 20) {
+				const proceed = await vscode.window.showWarningMessage(
+					`There are ${textFiles.length} files in the selected folder. Do you want to proceed?`,
+					{ modal: true },
+					'Yes',
+					'No'
+				);
+
+				// If the user chooses 'No', stop the operation
+				if (proceed !== 'Yes') {
+					return;
+				}
+			}
+
+			const markdownContent = generateMarkdownContent(textFiles);
+			const doc = await vscode.workspace.openTextDocument({
+				content: markdownContent,
+				language: 'markdown',
+			});
+			await vscode.window.showTextDocument(doc);
+		} catch (error) {
+			vscode.window.showErrorMessage(
+				`An error occurred while processing the files: ${error.message}`
+			);
+		}
+	} else {
+		vscode.window.showWarningMessage('No folder was selected.');
+	}
+}
+
 function activate(context) {
 	let disposable = vscode.commands.registerCommand(
 		'extension.folderCode',
-		async () => {
-			const folder = await vscode.window.showOpenDialog({
-				canSelectMany: false,
-				canSelectFiles: false,
-				canSelectFolders: true,
-				openLabel: 'Select Folder',
-			});
-
-			if (folder && folder[0]) {
-				try {
-					const textFiles = await readTextFiles(folder[0].fsPath);
-
-					if (textFiles.length === 0) {
-						vscode.window.showWarningMessage(
-							'No text files were found in the selected folder.'
-						);
-						return;
-					}
-
-					const markdownContent = generateMarkdownContent(textFiles);
-					const doc = await vscode.workspace.openTextDocument({
-						content: markdownContent,
-						language: 'markdown',
-					});
-					await vscode.window.showTextDocument(doc);
-				} catch (error) {
-					vscode.window.showErrorMessage(
-						`An error occurred while processing the files: ${error.message}`
-					);
-				}
-			} else {
-				vscode.window.showWarningMessage('No folder was selected.');
-			}
-		}
+		executeFolderCode
 	);
 
 	context.subscriptions.push(disposable);
